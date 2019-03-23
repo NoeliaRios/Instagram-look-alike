43ec8cd6bfbfc6ffaca671ea68ec97497afcd763
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-03-23 12:19:42 -0300

    rotten cat

---
+++ dom.js
@@ -0,0 +1,11 @@
+var texto = document.querySelector('p')
+console.log(texto)
+
+// texto.classList.add('rojo')
+
+function rellenarCorazon(){
+
+    var iconos = document.querySelector('i')
+iconos.classList.toggle('fas')
+
+}
