9e9de154b5e37ad4d9d1bd13c168fbacddb3aecf
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-03-23 13:02:46 -0300

    likes

--- dom.js
+++ dom.js
@@ -3,7 +3,19 @@ console.log(texto)
 
 // texto.classList.add('rojo')
 
+var click = 0;
+var likesPares = 0;
+
 function rellenarCorazon(){
+
+    click++
+
+    if (click%2==0) {
+        likesPares ++
+        console.log(likesPares)
+        var numeroDeLikes = document.querySelector('#likes') 
+        console.log(numeroDeLikes.innerHTML = likesPares)      
+    }  
 
     var iconos = document.querySelector('i')
 iconos.classList.toggle('fas')
